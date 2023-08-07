e939b31067e1fe40bc76ffee396a6126bae47678
Zineb112 <zbelassiri@gmail.con>
2023-08-07 14:47:23 -0700

    9-rectangle.py added

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -3,17 +3,17 @@
 
 
 class Rectangle:
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
         type(self).number_of_instances += 1
         self.width = width
         self.height = height
