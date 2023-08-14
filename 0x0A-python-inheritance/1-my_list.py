6f72accb72f638032bc7abae1c3d57b664399558
Zineb112 <zbelassiri@gmail.con>
2023-08-14 08:47:17 -0700

    1-my_list.py added

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""
+    A custom list class that inherits from
+    the built-in list class.
+    """
 
 
 class MyList(list):
     """
-    A custom list class that inherits from the built-in list class.
+sh: 1: q: not found
+    in ascending sorted order
     """
-
     def print_sorted(self):
-        """
-        Print the elements of the list in ascending sorted order.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
