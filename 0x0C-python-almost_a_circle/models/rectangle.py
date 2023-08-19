cbbdbed67f9fe4b2ea7b134204f3f132c7b4f04a
Zineb112 <zbelassiri@gmail.con>
2023-08-19 12:57:06 -0700

    models/rectangle.py updated, 6-main.py added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,9 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """Method to display the rectangle with '#' characters"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        """Override __str__ method to return a formatted string"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
