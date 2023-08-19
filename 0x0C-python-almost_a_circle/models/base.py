b2839030e2e44f7832645088ace27e5947a9cf42
Zineb112 <zbelassiri@gmail.con>
2023-08-19 15:27:58 -0700

    models/base.py updated, 16-main.py added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,6 @@
 
 import json
 
-
 class Base:
     """Base class"""
 
@@ -37,3 +36,11 @@ class Base:
 
         with open(filename, "w") as file:
             file.write(json_data)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
