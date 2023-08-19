b1fc9ba440ca79676612a5425bf7d04b52819eaf
Zineb112 <zbelassiri@gmail.con>
2023-08-19 13:34:04 -0700

    models/square.py updated, 11-main.py added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Square class that inherits from Rectangle"""
 
@@ -20,6 +21,17 @@ class Square(Rectangle):
         """Setter for size attribute"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Method to update attributes based on arguments"""
+        if args:
+            arg_names = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                if i < len(arg_names):
+                    setattr(self, arg_names[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """Override __str__ method to return a formatted string"""
