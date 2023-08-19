4ba96809ee3189cce4839965ff71699fd9b27e4b
Zineb112 <zbelassiri@gmail.con>
2023-08-19 13:24:56 -0700

    models/square.py, 9-main.py added

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Square module """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Override __str__ method to return a formatted string"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
