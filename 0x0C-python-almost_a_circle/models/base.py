4212fb7d31611cb3067c8ed997ef0745d07c06a3
Zineb112 <zbelassiri@gmail.con>
2023-08-19 16:16:42 -0700

    101-main.py added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """ Base module """
 
 import json
+import csv
+import turtle
 
 
 class Base:
@@ -10,7 +12,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor for Base"""
+        """Initialize Base instance"""
         if id is not None:
             self.id = id
         else:
@@ -19,53 +21,125 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Converts a list of dictionaries to a JSON string"""
+        """Convert a list of dictionaries to a JSON string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file"""
-        if list_objs is None:
-            list_objs = []
-
-        class_name = cls.__name__
-        filename = f"{class_name}.json"
-        json_data = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
-
+        """Save a list of objects to a JSON file"""
+        filename = cls.__name__ + ".json"
         with open(filename, "w") as file:
-            file.write(json_data)
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
-        """Returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
+        """Convert a JSON string to a list of dictionaries"""
+        if json_string is None or json_string == "":
             return []
-        else:
-            return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Creates an instance with all attributes already set"""
-        if cls.__name__ == "Rectangle":
-            dummy_instance = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy_instance = cls(1)
-
-        dummy_instance.update(**dictionary)
-        return dummy_instance
+        return json.loads(json_string)
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances from a JSON file"""
-        filename = f"{cls.__name__}.json"
-
+        """Load a list of objects from a JSON file"""
+        filename = cls.__name__ + ".json"
         try:
             with open(filename, "r") as file:
-                json_data = file.read()
-                dict_list = cls.from_json_string(json_data)
-                return [cls.create(**d) for d in dict_list]
+                json_str = file.read()
+                list_dicts = cls.from_json_string(json_str)
+                return [cls.create(**dict_obj) for dict_obj in list_dicts]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance with attributes already set"""
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)  # Create a dummy instance
+        elif cls.__name__ == "Square":
+            new_instance = cls(1)  # Create a dummy instance
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Save a list of objects to a CSV file"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("")
+            else:
+                writer = csv.writer(file)
+                for obj in list_objs:
+                    if cls.__name__ == "Rectangle":
+                        data = [obj.id, obj.width, obj.height, obj.x, obj.y]
+                    elif cls.__name__ == "Square":
+                        data = [obj.id, obj.size, obj.x, obj.y]
+                    writer.writerow(data)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Load a list of objects from a CSV file"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r") as file:
+                reader = csv.reader(file)
+                objects = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        obj = cls(int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[0]))
+                    elif cls.__name__ == "Square":
+                        obj = cls(int(row[1]), int(row[2]), int(row[3]), int(row[0]))
+                    objects.append(obj)
+                return objects
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        # Initialize Turtle graphics
+        screen = turtle.Screen()
+        screen.bgcolor("white")
+        screen.title("Shapes Drawing")
+
+        # Create a Turtle object
+        pen = turtle.Turtle()
+        pen.speed(1)
+
+        # Draw rectangles from the list
+        for rect in list_rectangles:
+            pen.penup()
+            pen.goto(rect.x, rect.y)
+            pen.pendown()
+            pen.color("blue")
+            pen.begin_fill()
+            for _ in range(2):
+                pen.forward(rect.width)
+                pen.left(90)
+                pen.forward(rect.height)
+                pen.left(90)
+            pen.end_fill()
+
+        # Draw squares from the list
+        for square in list_squares:
+            pen.penup()
+            pen.goto(square.x, square.y)
+            pen.pendown()
+            pen.color("red")
+            pen.begin_fill()
+            for _ in range(4):
+                pen.forward(square.size)
+                pen.left(90)
+            pen.end_fill()
+
+        # Close the Turtle graphics window when clicked
+        screen.exitonclick()
+
+
+if __name__ == "__main__":
+    pass
