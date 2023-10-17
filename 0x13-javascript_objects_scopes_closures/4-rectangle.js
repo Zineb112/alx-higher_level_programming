b157e47b9d1813254c326964278b0b50cf4281c7
Zineb112 <zbelassiri@gmail.con>
2023-10-17 15:21:35 -0700

    4-rectangle.js added

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,38 @@
 #!/usr/bin/node
-
+/**
+ * Check the parameters provided
+ */
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
     }
   }
 
-  rotate() {
-    // Exchange the width and height of the rectangle
-    const temp = this.width;
+  rotate () {
+    let temp = 0;
+    temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
-    // Multiply the width and height of the rectangle by 2
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
-
 module.exports = Rectangle;
