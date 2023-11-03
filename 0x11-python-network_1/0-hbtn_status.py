a2a3074fa323a1f6c1ef5dd2a60a38af91ff213c
Zineb112 <zbelassiri@gmail.con>
2023-11-03 12:46:21 -0700

    0-hbtn_status.py added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-
-print("Body response:")
-print("    - type:", type(content))
-print("    - content:", content)
-print("    - utf8 content:", content.decode('utf-8'))
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
